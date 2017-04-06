d2f7c59046a88c12e00279a25a1882db56e1edb9
J.W. Clark <jwclark@rockhursths.edu>
2017-04-06 00:47:33 -0500

    Auto yes on a prompt

--- Debian/Jessie/jessie-setup.sh
+++ Debian/Jessie/jessie-setup.sh
@@ -88,7 +88,7 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-po
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 3000
 
 # persist the IP tables
-sudo netfilter-persistent save
+sudo netfilter-persistent save -y
 
 # TODO
 # run node as service
