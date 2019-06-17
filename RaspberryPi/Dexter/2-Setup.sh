35433869c3001f2a1262af0f391ecf7449f3da45
J.W. Clark <jwclark@rockhursths.edu>
2019-06-16 20:55:58 -0500

    Fix VNC w/out keyboard and mouse

--- RaspberryPi/Dexter/2-Setup.sh
+++ RaspberryPi/Dexter/2-Setup.sh
@@ -21,3 +21,9 @@ sudo ./firmware_update.sh
 
 cd ~pi/Desktop/GrovePi/Software/Python
 sudo python setup.py install
+
+# If you do not have a keyboard or mouse connected to your Pi, VNC Server may not start automatically.
+# This is caused by the Pi not having enough entropy, thus preventing VNC Server and other services from starting.
+# To resolve this, you will need to install another entropy generator, for example:
+# https://help.realvnc.com/hc/en-us/articles/360021693151-VNC-Server-does-not-start-on-Raspberry-Pi-unless-a-mouse-keyboard-are-connected
+sudo apt-get install -y haveged
