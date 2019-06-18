676eeaed8ff6a9293232a4f21e7c3c8687283e99
J.W. Clark <jwclark@rockhursths.edu>
2019-06-17 21:58:06 -0500

    Create 3-Minecraft.sh

---
+++ RaspberryPi/Dexter/3-Minecraft.sh
@@ -0,0 +1,13 @@
+# Prerequisites
+# https://www.raspberrypi.org/forums/viewtopic.php?t=191857
+sudo apt-get update
+sudo apt-get upgrade
+sudo rpi-update
+sudo apt-get install libgles2-mesa libsdl2-dev
+
+# Point of this is to install Minecraft Pi edition
+
+# Download from, follow instructions to tar gz
+# https://www.minecraft.net/en-us/edition/pi/
+
+
