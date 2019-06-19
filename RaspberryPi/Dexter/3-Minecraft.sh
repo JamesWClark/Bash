902690e93ab424fbe715407c5546ba80a50d5f0e
J.W. Clark <jwclark@rockhursths.edu>
2019-06-19 13:47:09 -0500

    Update 3-Minecraft.sh

--- RaspberryPi/Dexter/3-Minecraft.sh
+++ RaspberryPi/Dexter/3-Minecraft.sh
@@ -1,13 +1,27 @@
-# Prerequisites
+# Prerequisites for Stretch OS
 # https://www.raspberrypi.org/forums/viewtopic.php?t=191857
 sudo apt-get update
 sudo apt-get upgrade
 sudo rpi-update
 sudo apt-get install libgles2-mesa libsdl2-dev
 
-# Point of this is to install Minecraft Pi edition
+# Download Minecraft Pi Edition
+wget https://www.minecraft.net/content/dam/minecraft/edition-pi/minecraft-pi-0.1.1.tar.gz
+tar -zxvf minecraft-pi-0.1.1.tar.gz
 
-# Download from, follow instructions to tar gz
-# https://www.minecraft.net/en-us/edition/pi/
+# Setup API
+mkdir ~/mcpi-api
+cp -r ~/mcpi/api/python/mcpi ~/mcpi-api/
 
+### Test it
+# # run the program
+# ./mcpi/minecraft-pi
+# cd ~/mcpi-api
+# wget https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/minecraft/api_test.py
+# python api_test.py
 
+### Get Started
+# https://projects.raspberrypi.org/en/projects/getting-started-with-minecraft-pi
+
+### API Docs
+# https://www.stuffaboutcode.com/p/minecraft-api-reference.html
